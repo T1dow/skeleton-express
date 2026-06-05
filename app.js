8ff632c27b77c58f9ab6ac7f275306821bb90956
NoMal <tidowephraim@gmail.com>
2026-06-05 03:00:55 -0700

    the is the end for destructures

---
+++ app.js
@@ -0,0 +1,10 @@
+const user = { first: 'John', last: 'Doe' , age: 30, email: 'john.doe@example.com' };
+// const { first, last, age, email } = user;
+// console.log(`Name: ${first} ${last}, Age: ${age}, Email: ${email}`);
+
+const color = ["red","green","yello"];
+const [firscolor, secondcolor] = color;
+
+console.log(firscolor);
+
+ 
